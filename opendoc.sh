0574cac9d681c9b6760a762067e26f458fed94e7
Aidan Bird <aidan@birdwebwork.com>
2021-07-06 14:04:21 -0400

    new file:   opendoc.sh

---
+++ opendoc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# aidan bird 2021
+#
+# list and open e-textbooks in ~/doc/text
+#
+
+doc_path="$(realpath ~/doc/text)/"
+sed_str_doc_path="$(echo "$doc_path" | sed 's/\//\\\//g')"
+sel="$(find "$doc_path" -type f -print0 | sed -z "s/$sed_str_doc_path//g" \
+    | fzf --read0)"
+[ -z "$sel" ] && { echo 'bad selection'; exit 1; }
+echo "$sel" | xargs -I '{}' sh -c "zathura \"$doc_path{}\" & disown"
+exit 0
